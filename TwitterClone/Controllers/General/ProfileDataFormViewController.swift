f754746cdc893a20864654cec3bcf15778d90be0
MukhammedAli <muhammedalikhamzayev@gmail.com>
2023-03-10 19:23:56 +0600

    ProfileDataViewController is completed

--- TwitterClone/Controllers/General/ProfileDataFormViewController.swift
+++ TwitterClone/Controllers/General/ProfileDataFormViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SnapKit
+import PhotosUI
 
 class ProfileDataFormViewController: UIViewController {
 
@@ -22,9 +23,23 @@ class ProfileDataFormViewController: UIViewController {
         textField.keyboardType = .default
         textField.backgroundColor = .secondarySystemFill
         textField.leftViewMode = .always
-       
+        textField.leftView = UIView(frame: CGRect(x: 0 , y: 0, width: 20, height: 20))
+        textField.layer.masksToBounds = true
+        textField.layer.cornerRadius  = 8
+        textField.attributedPlaceholder = NSAttributedString(string: "Display Name", attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
         return textField
-        
+    }()
+    
+    private let userNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.keyboardType = .default
+        textField.backgroundColor = .secondarySystemFill
+        textField.leftViewMode = .always
+        textField.leftView = UIView(frame: CGRect(x: 0 , y: 0, width: 20, height: 20))
+        textField.layer.masksToBounds = true
+        textField.layer.cornerRadius  = 8
+        textField.attributedPlaceholder = NSAttributedString(string: "Username", attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        return textField
     }()
     
     private let hintLabel: UILabel = {
@@ -35,15 +50,73 @@ class ProfileDataFormViewController: UIViewController {
         return label
     }()
     
+    private let avatarPlaceHolderImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = 60
+        imageView.backgroundColor = .lightGray
+        imageView.image = UIImage(systemName: "camera.fill")
+        imageView.tintColor = .black
+        imageView.isUserInteractionEnabled = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let bioTextView: UITextView = {
+       let textView = UITextView()
+        textView.backgroundColor = .secondarySystemFill
+        textView.layer.masksToBounds = true
+        textView.layer.cornerRadius = 8
+        textView.textContainerInset = .init(top: 15, left: 15, bottom: 15, right: 15)
+        textView.text = "Tell the world about yourself"
+        textView.textColor = .gray
+        textView.font = .systemFont(ofSize: 16)
+        return textView
+    }()
+    
+    private let submitButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Submit", for: .normal)
+        button.tintColor = .white
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .bold)
+        button.backgroundColor = UIColor(red: 29/255, green: 163/255, blue: 242/255, alpha: 1)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 25
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         view.addSubview(scrollView)
         scrollView.addSubview(hintLabel)
-        
+        scrollView.addSubview(avatarPlaceHolderImageView)
+        scrollView.addSubview(displayNameTextField)
+        scrollView.addSubview(userNameTextField)
+        scrollView.addSubview(bioTextView)
+        scrollView.addSubview(submitButton)
         isModalInPresentation = true
-        
+        bioTextView.delegate = self
+        displayNameTextField.delegate = self
+        userNameTextField.delegate = self
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapToDismiss)))
         configureConstraints()
+        avatarPlaceHolderImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapToUpload)))
+    }
+    
+    @objc private func didTapToUpload() {
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images
+        configuration.selectionLimit = 1
+        
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    @objc private func didTapToDismiss() {
+        view.endEditing(true)
     }
     
     private func configureConstraints() {
@@ -55,6 +128,43 @@ class ProfileDataFormViewController: UIViewController {
             $0.centerX.equalToSuperview()
             $0.top.equalToSuperview().inset(30)
         }
+        
+        avatarPlaceHolderImageView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(120)
+            $0.width.equalTo(120)
+            $0.top.equalTo(hintLabel.snp.bottom).inset(-30)
+        }
+        
+        displayNameTextField.snp.makeConstraints {
+            $0.left.equalToSuperview().inset(20)
+            $0.right.equalToSuperview().inset(-20)
+            $0.top.equalTo(avatarPlaceHolderImageView.snp.bottom).inset(-40)
+            $0.height.equalTo(50)
+            $0.width.equalTo(view.bounds.width - 40)
+        }
+        
+        userNameTextField .snp.makeConstraints {
+            $0.left.equalTo(displayNameTextField.snp.left)
+            $0.right.equalTo(displayNameTextField.snp.right)
+            $0.top.equalTo(displayNameTextField.snp.bottom).inset(-20)
+            $0.height.equalTo(50)
+            $0.width.equalTo(view.bounds.width - 40)
+        }
+        
+        bioTextView.snp.makeConstraints {
+            $0.left.equalTo(displayNameTextField.snp.left)
+            $0.right.equalTo(displayNameTextField.snp.right)
+            $0.top.equalTo(userNameTextField.snp.bottom).inset(-20)
+            $0.height.equalTo(150)
+        }
+        
+        submitButton.snp.makeConstraints {
+            $0.left.equalToSuperview().inset(20)
+            $0.right.equalToSuperview().inset(-20)
+            $0.height.equalTo(50)
+            $0.bottom.equalTo(view.keyboardLayoutGuide.snp.top).inset(-20)
+        }
     }
     
 
@@ -62,7 +172,48 @@ class ProfileDataFormViewController: UIViewController {
 }
 
 
+extension ProfileDataFormViewController: UITextViewDelegate , UITextFieldDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == .gray {
+            textView.textColor = .label
+            textView.text = ""
+        }
+        scrollView.setContentOffset(CGPoint(x: 0, y: textView.frame.origin.y - 100), animated: true)
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+        if textView.text.isEmpty {
+            textView.text = "Tell the world about yourself"
+            textView.textColor = .gray
+        }
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        scrollView.setContentOffset(CGPoint(x: 0, y: textField.frame.origin.y - 100), animated: true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+    }
+}
 
 
-
+extension ProfileDataFormViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        
+        for result in results {
+            result.itemProvider.loadObject(ofClass: UIImage.self) {[weak self] object, error in
+                if let image = object as? UIImage {
+                    DispatchQueue.main.async {
+                        self?.avatarPlaceHolderImageView.image = image
+                    }
+                }
+            }
+        }
+    }
+    
+    
+}
 
